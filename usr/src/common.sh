2a777577b889961d2509e365f7d9c7e199fcb7e7
Matej Kollar <208115@mail.muni.cz>
2015-01-03 01:34:57 +0100

    Separate builder for use during server sartup

--- usr/src/common.sh
+++ usr/src/common.sh
@@ -5,3 +5,19 @@ source "${OPENSHIFT_CARTRIDGE_SDK_BASH}"
 set -eu
 
 declare logfile=$( readlink -f "${OPENSHIFT_LOG_DIR}/ocaml_core.log" )
+
+function install_local_package() {
+    local dir="${1}"
+    ( cd "${dir}"
+        eval $( opam config env )
+        if [ -f _oasis ]; then
+            oasis setup
+        fi
+        declare pkgname=$( sed -n 's/name:\s*"\?\([^"]*\)"\?/\1/p' opam | head -1 )
+        if [ -d "${OPAMROOT}/${OCAMLVERSION}/packages.dev/${pkgname}" ]; then
+            opam upgrade "${pkgname}" -y |& tee -a "${logfile}"
+        else
+            opam pin add "${pkgname}" . -y |& tee -a "${logfile}"
+        fi
+    )
+}
