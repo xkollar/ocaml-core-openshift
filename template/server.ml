dbca56298d76bd908a89c7a3d532f2d8ca3f2558
Matej Kollar <208115@mail.muni.cz>
2015-01-31 09:51:12 +0100

    Experiment with JSON

--- template/server.ml
+++ template/server.ml
@@ -1,41 +1,92 @@
-let rec fact n = if n == 0 then 1 else n * fact (n-1)
+module Fact = struct
+  let rec fact n = if n == 0 then 1 else n * fact (n-1)
 
-let int_option_of_string s =
-  try
-    Some (int_of_string s)
-  with error -> None
+  let int_option_of_string s =
+    try
+      Some (int_of_string s)
+    with error -> None
 
-let generate_info_page (cgi : Netcgi.cgi_activation) =
-  let out = cgi # output # output_string in
-  out "?n=<int>"
+  let generate_info_page (cgi : Netcgi.cgi_activation) =
+    let out = cgi # output # output_string in
+    out "?n=<int>"
 
-let generate_result_page (cgi : Netcgi.cgi_activation) n =
-  let out = cgi # output # output_string in
-  out (fact n |> string_of_int)
+  let generate_result_page (cgi : Netcgi.cgi_activation) n =
+    let out = cgi # output # output_string in
+    out (fact n |> string_of_int)
 
-let generate_page (cgi : Netcgi.cgi_activation) =
-  match cgi # argument_value "n" |> int_option_of_string with
-  | None -> generate_info_page cgi
-  | Some n -> generate_result_page cgi n
+  let generate_page (cgi : Netcgi.cgi_activation) =
+    match cgi # argument_value "n" |> int_option_of_string with
+    | None -> generate_info_page cgi
+    | Some n -> generate_result_page cgi n
 
-let process (cgi : Netcgi.cgi_activation) =
-  cgi # set_header
-    ~cache:`No_cache
-    ~content_type:"text/plain; charset=\"iso-8859-1\""
-    ();
-  generate_page cgi;
-  cgi # output # commit_work()
+  let process (cgi : Netcgi.cgi_activation) =
+    cgi # set_header
+      ~cache:`No_cache
+      ~content_type:"text/plain; charset=\"iso-8859-1\""
+      ();
+    generate_page cgi;
+    cgi # output # commit_work()
 
-let fact_handler =
-  { Nethttpd_services.dyn_handler = (fun _ -> process);
-    dyn_activation = Nethttpd_services.std_activation `Std_activation_buffered;
-    dyn_uri = None; (* not needed *)
-    dyn_translator = (fun _ -> ""); (* not needed *)
-    dyn_accept_all_conditionals = false;
-  }
+  let handler =
+    { Nethttpd_services.dyn_handler = (fun _ -> process);
+      dyn_activation = Nethttpd_services.std_activation `Std_activation_buffered;
+      dyn_uri = None; (* not needed *)
+      dyn_translator = (fun _ -> ""); (* not needed *)
+      dyn_accept_all_conditionals = false;
+    }
+end
+
+module Webhook = struct
+  let get_json_request (cgi : Netcgi.cgi_activation) =
+    cgi # argument_value "BODY"
+    |> Yojson.Basic.from_string
+
+  let process_json_request jreq =
+    let dir =  Unix.getenv "OPENSHIFT_DATA_DIR" in
+
+    let oc = open_out_gen [Open_append; Open_creat] 0o600 (dir ^ "hooklog") in
+
+    let open Yojson.Basic.Util in
+
+    jreq |> member "action" |> to_string
+    |> Printf.fprintf oc "%s\n";
+    jreq |> Yojson.Basic.to_string
+    |> Printf.fprintf oc "%s\n";
+    close_out oc
+
+  let generate_page (cgi : Netcgi.cgi_activation) =
+    get_json_request cgi |> process_json_request;
+    `Assoc ["response", `String "noted"]
+    |> Yojson.Basic.to_output cgi # out_channel
+
+  let process (cgi : Netcgi.cgi_activation) =
+    cgi # set_header
+      ~cache:`No_cache
+      ~content_type:"application/json; charset=UTF-8"
+      ();
+    generate_page cgi;
+    cgi # out_channel # commit_work ()
+
+  let handler =
+    { Nethttpd_services.dyn_handler = (fun _ -> process);
+      dyn_activation = Nethttpd_services.std_activation `Std_activation_buffered;
+      dyn_uri = None; (* not needed *)
+      dyn_translator = (fun _ -> ""); (* not needed *)
+      dyn_accept_all_conditionals = false;
+    }
+end
 
 let service_factory = Nethttpd_plex.nethttpd_factory
-    ~handlers:[ "fact_service", fact_handler ]
+  ~config_cgi:Netcgi.({
+    default_config with
+    default_exn_handler = false;
+    permitted_input_content_types = [
+      "application/json";
+    ] @ default_config.permitted_input_content_types})
+  ~handlers:[
+    "fact_service", Fact.handler;
+    "hook_service", Webhook.handler
+  ]
 
 let main () =
   (* Decault config, to be overwritten by command-line arguments *)
@@ -69,6 +120,7 @@ let main () =
     cfg
 
 let () =
+  (*
   Sys.getcwd ()
   |> Printf.printf "CWD: %s\n%!";
 
@@ -76,6 +128,7 @@ let () =
   |> Array.to_list
   |> String.concat "\n"
   |> Printf.printf "Running in environment:\n%s\n\n%!";
+  *)
 
   Sys.set_signal Sys.sigpipe Sys.Signal_ignore;
   if not !Sys.interactive
